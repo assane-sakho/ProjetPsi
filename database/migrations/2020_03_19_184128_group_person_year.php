b7ac499e908dc6380277ca5fdf15192dfb345c24
assanes <assanesakho.pro@gmail.com>
2020-03-19 19:48:10 +0100

    [ADD] - Migration

---
+++ database/migrations/2020_03_19_184128_group_person_year.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class GroupPersonYear extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_person_year', function (Blueprint $table) {
+            $table->unsignedBigInteger('group_id');
+            $table->unsignedBigInteger('person_id');
+            $table->unsignedBigInteger('year');
+
+            $table->foreign('group_id')->references('id')->on('groups');
+            $table->foreign('person_id')->references('id')->on('people');
+            $table->foreign('year')->references('year')->on('school_years');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_person_year');
+    }
+}
