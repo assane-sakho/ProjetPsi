4d016b219caf5b0b6720197b77e1a37220650a37
ilhame5 <ilhame.92700@gmail.com>
2020-03-20 20:10:40 +0100

    [UPDATE] - add, modify, delete Person

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -15,5 +15,38 @@ class PersonController extends Controller
       
         return view('person.partial', compact('people'));
     }
+    function add(Request $request)
+    {
+        $id = $request->input("addId");
+        $lastName = $request->input("addLastname");
+        $firstName = $request->input("addFirstname");
+        $email = $request->input("addEmail");
+        $num=$request->input("addNum");
+        $idDirectory=$request->input("addDirectory");
+        $idStatus=$request->input("addStatus");
+
+        $groupe = Person::create(['lastname' => $lastName,'firstname' => $firstName,'email' => $email,'num' => $num, 'directory_id' => $idDirectory, 'status_id' => $idStatus]);
+        return true;
+    }
+    function update(Request $request)
+    {
+        $id = $request->input("editId");
+        $lastName = $request->input("editLastname");
+        $firstName = $request->input("editFirstname");
+        $email = $request->input("editEmail");
+        $num=$request->input("editNum");
+        $idDirectory=$request->input("editDirectory");
+        $idStatus=$request->input("editStatus");
+
+        $group = Person::where('id',$id);
+        $group-> update(['lastname' => $lastName,'firstname' => $firstName,'email' => $email,'num' => $num, 'directory_id' => $idDirectory, 'status_id' => $idStatus]);
+    }
+
+    function delete(Request $request)
+    {
+        $id = $request->input("deleteId");
+        $group = Person::where('id',$id);
+        $group-> delete();
+    }
 
 }
